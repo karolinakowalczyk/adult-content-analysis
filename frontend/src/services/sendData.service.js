1c6c741112d63981a5dee168f7d76ce95ee0a0a0
ptroszcz <61745721+ptroszcz@users.noreply.github.com>
2023-06-09 18:18:14 +0200

    Added sending file

--- frontend/src/services/sendData.service.js
+++ frontend/src/services/sendData.service.js
@@ -1,15 +1,33 @@
 import api from './api'
 
-const sendDataWithUrlForVideoAudioAnalysis = (url, email) => {
-    return api.post(`/youtube_video_audio?youtube-url=${url}&email=${email}`)
+const sendDataWithUrlForVideoAudioAnalysis = (url, email, file) => {
+    let formdata = new FormData();
+    formdata.append('file',file[0]);
+    return api.post(`/youtube_video_audio?youtube-url=${url}&email=${email}`, formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }
+      })
 }
 
-const sendDataWithUrlForAudioAnalysis = (url, email) => {
-    return api.post(`/youtube_audio?youtube-url=${url}&email=${email}`)
+const sendDataWithUrlForAudioAnalysis = (url, email, file) => {
+    let formdata = new FormData();
+    formdata.append('file',file[0]);
+    return api.post(`/youtube_audio?youtube-url=${url}&email=${email}`, formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }
+      })
 }
 
-const sendDataWithUrlForVideoAnalysis = (url, email) => {
-    return api.post(`/youtube_video?youtube-url=${url}&email=${email}`)
+const sendDataWithUrlForVideoAnalysis = (url, email, file) => {
+    let formdata = new FormData();
+    formdata.append('file',file[0]);
+    return api.post(`/youtube_video?youtube-url=${url}&email=${email}`, formdata, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }
+      })
 }
 
 const sendDataService = {
